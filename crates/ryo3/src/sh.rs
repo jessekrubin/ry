1ae309a8123531228d20c72e745fe5fad1ed6550
jessekrubin <jessekrubin@gmail.com>
2024-01-19 10:14:27 -0800

    fix thingy

--- crates/ryo3/src/sh.rs
+++ crates/ryo3/src/sh.rs
@@ -42,9 +42,33 @@ pub fn cd(
     }
 }
 
+#[pyfunction]
+pub fn ls(fspath: Option<PathLike>) -> PyResult<Vec<String>> {
+    let p = fspath.unwrap_or_else(|| PathLike::PathBuf(std::env::current_dir().unwrap()));
+    let r = std::fs::read_dir(p.as_ref());
+    match r {
+        Ok(r) => {
+            let v = r
+                .map(|x| x.unwrap())
+                .map(|y| y.path())
+                .map(|z| z.file_name().unwrap().to_str().unwrap().to_string())
+                .collect();
+            Ok(v)
+        }
+        Err(e) => {
+            let p_string = String::from(p);
+            // let p_string = p.to_str().unwrap().to_string();
+            let emsg = format!("{}: {:?}", e.to_string(), p_string);
+            let pye = PyFileNotFoundError::new_err(format!("ls: {}", emsg));
+            Err(pye)
+        }
+    }
+}
+
 pub fn madd(m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(pwd, m)?)?;
     m.add_function(wrap_pyfunction!(cd, m)?)?;
     m.add_function(wrap_pyfunction!(home, m)?)?;
+    m.add_function(wrap_pyfunction!(ls, m)?)?;
     Ok(())
 }
