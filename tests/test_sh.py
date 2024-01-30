07d12348db06c5bd06099220abf690014c432334
jessekrubin <jessekrubin@gmail.com>
2024-01-30 15:40:04 -0800

    test sh

--- tests/test_sh.py
+++ tests/test_sh.py
@@ -4,11 +4,11 @@ import ry
 def test_ls(tmpdir):
     tmpdir.join("a.txt").write("hello")
     tmpdir.join("b.txt").write("world")
-    assert ry.ls(tmpdir) == ["a.txt", "b.txt"]
+    assert set(ry.ls(tmpdir)) == {"a.txt", "b.txt"}
 
 
 def test_ls_pathlib(tmpdir):
     ry.cd(tmpdir)
     tmpdir.join("a.txt").write("hello")
     tmpdir.join("b.txt").write("world")
-    assert ry.ls() == ["a.txt", "b.txt"]
+    assert set(ry.ls()) == {"a.txt", "b.txt"}
