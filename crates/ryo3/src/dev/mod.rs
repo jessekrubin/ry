f831c3fce0c64ebd71a8a6f03ebdfef322af80cb
jessekrubin <jessekrubin@gmail.com>
2024-02-22 15:25:53 -0800

    quick maths

--- crates/ryo3/src/dev/mod.rs
+++ crates/ryo3/src/dev/mod.rs
@@ -3,11 +3,14 @@ use pyo3::prelude::*;
 use pyo3::{wrap_pyfunction, PyResult, Python};
 
 #[pyfunction]
-pub fn rydev() -> PyResult<String> {
-    Ok("RYDEV".to_string())
+pub fn quick_maths() -> i32 {
+    // 2 + 2 that's 4, minus 1 that's 3, quick maths
+    let mut qm = 2 + 2;
+    qm -= 1;
+    qm
 }
 
 pub fn madd(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(rydev, m)?)?;
+    m.add_function(wrap_pyfunction!(quick_maths, m)?)?;
     Ok(())
 }
