9dddafed03806eff558265b3737610a9b47df513
jessekrubin <jessekrubin@gmail.com>
2024-02-29 07:49:02 -0800

    json primitive et al

--- python/ry/_ry.pyi
+++ python/ry/_ry.pyi
@@ -6,6 +6,16 @@ __version__: str
 __build_profile__: str
 __build_timestamp__: str
 
+# ==============================================================================
+# TYPE ALIASES
+# ==============================================================================
+JsonPrimitive = None | bool | int | float | str
+JsonValue = (
+    JsonPrimitive
+    | dict[str, JsonPrimitive | JsonValue]
+    | list[JsonPrimitive | JsonValue]
+)
+
 class FsPath:
     def __init__(self, path: str | None) -> None: ...
     def __str__(self) -> str: ...
@@ -18,6 +28,11 @@ FsPathLike = str | FsPath | PathLike
 def pwd() -> str: ...
 def cd(path: FsPathLike) -> None: ...
 def ls(path: FsPathLike | None) -> list[FsPath]: ...
+
+# ==============================================================================
+# JSON
+# ==============================================================================
+def parse_json(input: str | bytes) -> JsonValue: ...
 
 # ==============================================================================
 # FORMATTING
