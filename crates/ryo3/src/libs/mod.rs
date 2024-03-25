7ca611687ea4c7ae13b07d18c7e7f5eac186d44e
jesse <jessekrubin@gmail.com>
2024-03-25 11:52:23 -0700

    brotli!

--- crates/ryo3/src/libs/mod.rs
+++ crates/ryo3/src/libs/mod.rs
@@ -4,9 +4,15 @@ use pyo3::{PyResult, Python};
 #[cfg(feature = "jiter")]
 mod jiter_ry;
 
+#[cfg(feature = "brotli")]
+mod brotli;
+
 pub fn madd(_py: Python, _m: &PyModule) -> PyResult<()> {
     #[cfg(feature = "jiter")]
     jiter_ry::madd(_py, _m)?;
+
+    #[cfg(feature = "brotli")]
+    brotli::madd(_py, _m)?;
 
     Ok(())
 }
