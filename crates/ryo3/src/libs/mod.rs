fac5cb7c0113afa17172b1f4e2b3a4f6bebd9aa0
jessekrubin <jessekrubin@gmail.com>
2024-11-26 11:11:32 -0800

    ryo3-heck

--- crates/ryo3/src/libs/mod.rs
+++ crates/ryo3/src/libs/mod.rs
@@ -5,6 +5,16 @@ mod walkdir;
 pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
     #[cfg(feature = "brotli")]
     ryo3_brotli::pymod_add(m)?;
+    #[cfg(feature = "bzip2")]
+    ryo3_bzip2::pymod_add(m)?;
+    #[cfg(feature = "flate2")]
+    ryo3_flate2::pymod_add(m)?;
+    #[cfg(feature = "fnv")]
+    ryo3_fnv::pymod_add(m)?;
+    #[cfg(feature = "globset")]
+    ryo3_globset::pymod_add(m)?;
+    #[cfg(feature = "heck")]
+    ryo3_heck::pymod_add(m)?;
     #[cfg(feature = "jiff")]
     ryo3_jiff::pymod_add(m)?;
     #[cfg(feature = "jiter")]
@@ -15,30 +25,15 @@ pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
     ryo3_sqlformat::pymod_add(m)?;
     #[cfg(feature = "which")]
     ryo3_which::pymod_add(m)?;
-
-    // TODO: move the below libs to their own crates
-    #[cfg(feature = "bzip2")]
-    ryo3_bzip2::pymod_add(m)?;
-
-    #[cfg(feature = "fnv")]
-    ryo3_fnv::pymod_add(m)?;
-
-    #[cfg(feature = "flate2")]
-    ryo3_flate2::pymod_add(m)?;
-
-    #[cfg(feature = "globset")]
-    ryo3_globset::pymod_add(m)?;
-
-    #[cfg(feature = "walkdir")]
-    walkdir::pymod_add(m)?;
-
-    // #[cfg(feature = "xxhash")]
-    // xxhash::pymod_add(m)?;
     #[cfg(feature = "xxhash")]
     ryo3_xxhash::pymod_add(m)?;
-
     #[cfg(feature = "zstd")]
     ryo3_zstd::pymod_add(m)?;
+
+
+    // TODO: move the below libs to their own crates
+    #[cfg(feature = "walkdir")]
+    walkdir::pymod_add(m)?;
 
     Ok(())
 }
