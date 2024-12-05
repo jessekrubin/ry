50ce458aa4cbe2caed10f580f84982af0acd520d
jessekrubin <jessekrubin@users.noreply.github.com>
2024-12-05 17:24:23 +0000

    deploy: 9f0a86417077ee7afbfef559f3c7729804634f2e

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="index.html"><strong aria-hidden="true">1.</strong> README</a></li><li class="chapter-item expanded "><a href="DEVELOPMENT.html"><strong aria-hidden="true">2.</strong> DEVELOPMENT</a></li><li class="chapter-item expanded "><a href="API.html"><strong aria-hidden="true">3.</strong> API</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded "><a href="index.html"><strong aria-hidden="true">1.</strong> README</a></li><li class="chapter-item expanded "><a href="DEVELOPMENT.html"><strong aria-hidden="true">2.</strong> DEVELOPMENT</a></li><li class="chapter-item expanded affix "><li class="spacer"></li><li class="chapter-item expanded "><a href="api.html"><strong aria-hidden="true">3.</strong> API</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString();
         if (current_page.endsWith("/")) {
