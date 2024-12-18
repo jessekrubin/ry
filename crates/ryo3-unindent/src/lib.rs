dfd6dcda92c72393dd231ddd2bed682b18bd05b2
jessekrubin <jessekrubin@gmail.com>
2024-12-18 07:51:23 -0800

    clippy fix

--- crates/ryo3-unindent/src/lib.rs
+++ crates/ryo3-unindent/src/lib.rs
@@ -3,11 +3,13 @@ use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyModule};
 
 #[pyfunction]
+#[must_use]
 pub fn unindent(input: &str) -> String {
     ::unindent::unindent(input)
 }
 
 #[pyfunction]
+#[must_use]
 pub fn unindent_bytes<'py>(py: Python<'py>, input: &[u8]) -> Bound<'py, PyBytes> {
     let b = ::unindent::unindent_bytes(input);
     let pb = PyBytes::new(py, &b);
