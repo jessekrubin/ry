30092ceffa4bcd4a6c43bb06beb2a211d6df8855
jessekrubin <jessekrubin@gmail.com>
2024-12-20 10:49:31 -0800

    bytes and reqwest work in progress!

--- crates/ryo3-reqwest/src/async_client.rs
+++ crates/ryo3-reqwest/src/async_client.rs
@@ -1,11 +1,14 @@
 use pyo3::prelude::*;
 
 use crate::errors::map_reqwest_err;
-use crate::pyo3_bytes::{Pyo3Bytes, Pyo3JsonBytes};
+use crate::pyo3_bytes::Pyo3JsonBytes;
 use bytes::Bytes;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use pyo3::types::PyDict;
 use reqwest::StatusCode;
+use ryo3_bytes::Pyo3Bytes;
+use ryo3_url::PyUrl;
 use std::borrow::Borrow;
 
 #[pyclass]
@@ -49,6 +52,29 @@ impl RyAsyncResponse {
     #[getter]
     fn status_code(&self) -> PyResult<u16> {
         Ok(self.status_code.as_u16())
+    }
+
+    #[getter]
+    #[pyo3(name = "url")]
+    fn py_url(&self) -> PyUrl {
+        PyUrl(self.url.clone())
+    }
+
+    #[getter]
+    #[pyo3(name = "headers")]
+    fn py_headers<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyDict>> {
+        let c = self.headers.clone();
+        let pydict = PyDict::new(py);
+        for (name, value) in c.iter() {
+            let k = name.to_string();
+            let v = value
+                .to_str()
+                .map(String::from)
+                .map_err(|e| PyValueError::new_err(format!("{e}")))?;
+            // .to_str()?.to_string();
+            pydict.set_item(k, v)?;
+        }
+        Ok(pydict)
     }
 
     fn bytes<'py>(&'py mut self, py: Python<'py>) -> PyResult<Bound<'py, PyAny>> {
@@ -107,6 +133,28 @@ impl RyAsyncClient {
     // self.request(Method::GET, url)
     fn get<'py>(&'py mut self, py: Python<'py>, url: String) -> PyResult<Bound<'py, PyAny>> {
         let response_future = self.0.get(&url).send();
+        pyo3_async_runtimes::tokio::future_into_py(py, async move {
+            let response = response_future
+                .await
+                .map_err(|e| PyValueError::new_err(format!("{e}")))?;
+            let r = RyAsyncResponse {
+                status_code: response.status(),
+                headers: response.headers().clone(),
+                url: response.url().clone(),
+                body: None,
+                res: Some(response),
+            };
+            Ok(r)
+        })
+    }
+
+    fn post<'py>(
+        &'py mut self,
+        py: Python<'py>,
+        url: String,
+        body: &[u8],
+    ) -> PyResult<Bound<'py, PyAny>> {
+        let response_future = self.0.post(&url).body(body.to_vec()).send();
         pyo3_async_runtimes::tokio::future_into_py(py, async move {
             let response = response_future
                 .await
--- crates/ryo3-reqwest/src/pyo3_bytes.rs
+++ crates/ryo3-reqwest/src/pyo3_bytes.rs
@@ -1,30 +1,29 @@
 use bytes::Bytes;
 use jiter::{map_json_error, PythonParse};
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
-
-pub(crate) struct Pyo3Bytes(pub Bytes);
-
-impl Pyo3Bytes {
-    pub fn new(buf: Bytes) -> Self {
-        Self(buf)
-    }
-}
-impl From<Bytes> for Pyo3Bytes {
-    fn from(value: Bytes) -> Self {
-        Self::new(value)
-    }
-}
-
-impl<'py> IntoPyObject<'py> for Pyo3Bytes {
-    type Target = PyBytes;
-    type Output = Bound<'py, Self::Target>;
-    type Error = PyErr;
-
-    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        Ok(PyBytes::new(py, &self.0[..]))
-    }
-}
+//
+// pub(crate) struct Pyo3Bytes(pub Bytes);
+//
+// impl Pyo3Bytes {
+//     pub fn new(buf: Bytes) -> Self {
+//         Self(buf)
+//     }
+// }
+// impl From<Bytes> for Pyo3Bytes {
+//     fn from(value: Bytes) -> Self {
+//         Self::new(value)
+//     }
+// }
+//
+// impl<'py> IntoPyObject<'py> for Pyo3Bytes {
+//     type Target = PyBytes;
+//     type Output = Bound<'py, Self::Target>;
+//     type Error = PyErr;
+//
+//     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+//         Ok(PyBytes::new(py, &self.0[..]))
+//     }
+// }
 
 pub(crate) struct Pyo3JsonBytes(pub Bytes);
 
