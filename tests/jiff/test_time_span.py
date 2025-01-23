e02aa91eb958ba4ce40e4bbccb4a79dfb69b7c9a
jessekrubin <jessekrubin@gmail.com>
2025-01-23 10:39:03 -0800

    mypy fixes

--- tests/jiff/test_time_span.py
+++ tests/jiff/test_time_span.py
@@ -234,91 +234,6 @@ class TestSpanCompare:
 
 
 class TestSpanTotal:
-    """
-    pub fn total<'a, T: Into<SpanTotal<'a>>>(
-        &self,
-        options: T,
-    ) -> Result<f64, Error>
-    Returns a floating point number representing the total number of a specific unit (as given) in this span. If the span is not evenly divisible by the requested units, then the number returned may have a fractional component.
-
-    This routine accepts anything that implements Into<SpanTotal>. There are some trait implementations that make using this routine ergonomic:
-
-    From<Unit> for SpanTotal computes a total for the given unit in this span.
-    From<(Unit, civil::Date)> for SpanTotal computes a total for the given unit in this span, relative to the given date. There are also From implementations for civil::DateTime and Zoned.
-    Errors
-    If this span has any non-zero calendar unit (units bigger than days), then this routine requires a relative datetime. If one is not provided, then an error is returned.
-
-    An error can also occur when adding the span to the relative datetime given results in overflow.
-
-    Note that in jiff 0.2, this routine will return an error if no relative reference time is given and either of the spans have non-zero units of days or greater. Callers may write forward compatible code that assumes days are an invariant 24 hours in length by providing SpanRelativeTo::days_are_24_hours.
-
-    Example
-    This example shows how to find the number of seconds in a particular span:
-
-    use jiff::{ToSpan, Unit};
-
-    let span = 3.hours().minutes(10);
-    assert_eq!(span.total(Unit::Second)?, 11_400.0);
-    Example: 24 hour days
-    This shows how to find the total number of 24 hour days in 123,456,789 seconds.
-
-    use jiff::{ToSpan, Unit};
-
-    let span = 123_456_789.seconds();
-    assert_eq!(span.total(Unit::Day)?, 1428.8980208333332);
-    Example: DST is taken into account
-    The month of March 2024 in America/New_York had 31 days, but one of those days was 23 hours long due a transition into daylight saving time:
-
-    use jiff::{civil::date, ToSpan, Unit};
-
-    let span = 744.hours();
-    let relative = date(2024, 3, 1).in_tz("America/New_York")?;
-    // Because of the short day, 744 hours is actually a little *more* than
-    // 1 month starting from 2024-03-01.
-    assert_eq!(span.total((Unit::Month, &relative))?, 1.0013888888888889);
-    Now compare what happens when the relative datetime is civil and not time zone aware:
-
-    use jiff::{civil::date, ToSpan, Unit};
-
-    let span = 744.hours();
-    let relative = date(2024, 3, 1);
-    assert_eq!(span.total((Unit::Month, relative))?, 1.0);
-    Example: infallible sorting
-    The sorting example in Span::compare has to use unwrap() in its sort_by(..) call because Span::compare may fail and there is no “fallible” sorting routine in Rust’s standard library (as of 2024-07-07). While the ways in which Span::compare can fail for a valid configuration limited to overflow for “extreme” values, it is possible to sort spans infallibly by computing floating point representations for each span up-front:
-
-    use jiff::{ToSpan, Unit, Zoned};
-
-    let span1 = 79.hours().minutes(10);
-    let span2 = 3.days().hours(7).seconds(630);
-    let span3 = 3.days().hours(6).minutes(50);
-
-    let relative: Zoned = "2020-11-01T00-07[America/Los_Angeles]".parse()?;
-    let mut spans = [
-        (span1, span1.total((Unit::Day, &relative))?),
-        (span2, span2.total((Unit::Day, &relative))?),
-        (span3, span3.total((Unit::Day, &relative))?),
-    ];
-    spans.sort_by(|&(_, total1), &(_, total2)| total1.total_cmp(&total2));
-    assert_eq!(
-        spans.map(|(sp, _)| sp.fieldwise()),
-        [span1.fieldwise(), span3.fieldwise(), span2.fieldwise()],
-    );
-
-    // Compare with the result of sorting without taking DST into account.
-    // We can do that here since days are considered 24 hours long in all
-    // cases when no relative datetime is provided:
-    let mut spans = [
-        (span1, span1.total(Unit::Day)?),
-        (span2, span2.total(Unit::Day)?),
-        (span3, span3.total(Unit::Day)?),
-    ];
-    spans.sort_by(|&(_, total1), &(_, total2)| total1.total_cmp(&total2));
-    assert_eq!(
-        spans.map(|(sp, _)| sp.fieldwise()),
-        [span3.fieldwise(), span1.fieldwise(), span2.fieldwise()],
-    );
-    """
-
     def test_example(self) -> None:
         """
         ```rust
@@ -330,3 +245,51 @@ class TestSpanTotal:
         """
         span = ry.timespan(hours=3, minutes=10)
         assert span.total("second") == 11_400.0
+
+    def test_example_24_hour_days(self) -> None:
+        """
+        ```rust
+        use jiff::{ToSpan, Unit};
+
+        let span = 123_456_789.seconds();
+        assert_eq!(span.total(Unit::Day)?, 1428.8980208333332);
+        ```
+        """
+        span = ry.timespan(seconds=123_456_789)
+        assert span.total("day") == 1428.8980208333332
+
+    def test_example_DST_is_taken_into_account(self) -> None:
+        """
+        ```rust
+        use jiff::{civil::date, ToSpan, Unit};
+
+        let span = 744.hours();
+        let relative = date(2024, 3, 1).in_tz("America/New_York")?;
+        // Because of the short day, 744 hours is actually a little *more* than
+        // 1 month starting from 2024-03-01.
+        assert_eq!(span.total((Unit::Month, &relative))?, 1.0013888888888889);
+        ```
+        """
+        span = ry.timespan(hours=744)
+        relative = ry.Date(2024, 3, 1).in_tz("America/New_York")
+        assert span.total("month", relative) == 1.0013888888888889
+
+    def test_example_infallible_sorting(self) -> None:
+        span1 = ry.timespan(hours=79, minutes=10)
+        span2 = ry.timespan(days=3, hours=7, seconds=630)
+        span3 = ry.timespan(days=3, hours=6, minutes=50)
+        relative = ry.ZonedDateTime.parse("2020-11-01T00-07[America/Los_Angeles]")
+        spans = [
+            (span1, span1.total("day", relative)),
+            (span2, span2.total("day", relative)),
+            (span3, span3.total("day", relative)),
+        ]
+        spans_sorted = sorted(spans, key=lambda x: x[1])
+        assert [x[0] for x in spans_sorted] == [span1, span3, span2]
+        spans = [
+            (span1, span1.total("day")),
+            (span2, span2.total("day")),
+            (span3, span3.total("day")),
+        ]
+        spans_sorted_no_dst = sorted(spans, key=lambda x: x[1])
+        assert [x[0] for x in spans_sorted_no_dst] == [span3, span1, span2]
