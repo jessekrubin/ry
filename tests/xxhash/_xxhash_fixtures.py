85f095443bfe8c61189e23a41b22f218910c3515
jesse <jessekrubin@gmail.com>
2025-02-13 13:50:24 -0800

    Xxhash test cleanup (#200)
    * clean up of the rounding functions
    
    * fix: time span math
    
    * fix: type annotations
    
    * cleanup xxhash tests

---
+++ tests/xxhash/_xxhash_fixtures.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+from pathlib import Path
+from typing import TypedDict
+
+import ry
+
+_PWD = Path(__file__).parent.absolute()
+_TEST_DATA = _PWD / "xxhash.ndjson"
+
+XX32_SEEDS = [0, 1, 2**32 - 1]
+XX64_SEEDS = [0, 1, 2**64 - 1]
+XX128_SEEDS = [0, 1, 2**64 - 1]  # same as 64-bit seeds
+
+
+class XXHashDataRecord(TypedDict):
+    buf: str
+    xxh32_0x00000000: str
+    xxh32_0x00000001: str
+    xxh32_0xFFFFFFFF: str
+    xxh64_0x00000000: str
+    xxh64_0x00000001: str
+    xxh64_0xFFFFFFFFFFFFFFFF: str
+    xxh3_64_0x00000000: str
+    xxh3_64_0x00000001: str
+    xxh3_64_0xFFFFFFFFFFFFFFFF: str
+    xxh3_128_0x00000000: str
+    xxh3_128_0x00000001: str
+    xxh3_128_0xFFFFFFFFFFFFFFFF: str
+
+
+def _load_data():
+    with open(_TEST_DATA, "r") as f:
+        xx32_test_data = f.read()
+    lines = xx32_test_data.split("\n")
+    return [
+        XXHashDataRecord(
+            **row,
+        )
+        for row in (
+            ry.parse_json(line) for line in lines if line.strip() if line.strip()
+        )
+    ]
+
+
+XXHASH_TEST_DATA = _load_data()
