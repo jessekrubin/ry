58c953d2278538bc58826e5835e64f421d263152
jessekrubin <jessekrubin@gmail.com>
2025-02-20 12:23:00 -0800

    cleanup fs tokio module

--- crates/ryo3-tokio/src/fs.rs
+++ crates/ryo3-tokio/src/fs.rs
@@ -1,53 +1,29 @@
 //! python `tokio::fs` module
 
-use pyo3::exceptions::PyNotADirectoryError;
 use pyo3::prelude::*;
-use pyo3::IntoPyObjectExt;
-use ryo3_bytes::{extract_bytes_ref_str, PyBytes};
-use std::path::{Path, PathBuf};
+use ryo3_bytes::PyBytes;
+use std::path::PathBuf;
 
 #[allow(clippy::needless_pass_by_value)]
 #[pyfunction]
-pub fn read_async<'py>(py: Python<'py>, pth: PathBuf) -> PyResult<Bound<'py, PyAny>> {
+pub fn read_async(py: Python<'_>, pth: PathBuf) -> PyResult<Bound<'_, PyAny>> {
     pyo3_async_runtimes::tokio::future_into_py(py, async move {
-        let fbytes = tokio::fs::read(pth)
+        tokio::fs::read(pth)
             .await
             .map(ryo3_bytes::PyBytes::from)
-            .map_err(|e| {
-                PyErr::new::<pyo3::exceptions::PyOSError, _>(format!("read_async - {:?}", e))
-            });
-        fbytes
-        // let b = ryo3_bytes::PyBytes::from(fbytes);
-        // b
+            .map_err(|e| PyErr::from(e))
     })
 }
+
 #[allow(clippy::needless_pass_by_value)]
 #[pyfunction]
-pub fn write_async<'py>(
-    py: Python<'py>,
-    fspath: PathBuf,
-    // b: &Bound<'py, PyAny>,
-    b: PyBytes,
-) -> PyResult<Bound<'py, PyAny>> {
-    // let bref = extract_bytes_ref_str(b)?;
-    // let write_res = std::fs::write(fspath.as_ref(), bref);
-    // match write_res {
-    //     Ok(()) => Ok(bref.len()),
-    //     Err(e) => Err(PyNotADirectoryError::new_err(format!(
-    //         "write_bytes - parent: {fspath} - {e}"
-    //     ))),
-    // }
-
-    // let bref = extract_bytes_ref_str(b)?;
+pub fn write_async(py: Python<'_>, fspath: PathBuf, b: PyBytes) -> PyResult<Bound<'_, PyAny>> {
     pyo3_async_runtimes::tokio::future_into_py(py, async move {
         let bref: &[u8] = b.as_ref();
         let len = bref.len();
-        let write_res = tokio::fs::write(fspath, b).await.map(|_| len).map_err(|e| {
-            PyErr::new::<pyo3::exceptions::PyOSError, _>(format!("write_async - {:?}", e))
-        });
-        // write_res.map(|len| len.into_py(py))
-        Ok(())
-
-        // py.None().into_py_any(py)
+        tokio::fs::write(fspath, b)
+            .await
+            .map(|()| len)
+            .map_err(|e| PyErr::from(e))
     })
 }
