42be7de12409dd7280b219354141ca18e0d5b335
jesse <jessekrubin@gmail.com>
2025-04-22 17:45:14 -0700

    Fnv cleanup (#246)
    * cleanup fnv hashing
    
    * fixup fnv
    
    * brotli and friends updated

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,17 @@
 # CHANGELOG
 
+## v0.0.42 [unreleased]
+
+- `ryo3-fnv`
+  - align with hashlib style hashing
+- deps-up
+  - pyo3 version 0.24.2
+  - brotli 8
+  - jiff patch
+
+
+---
+
 ## v0.0.41 [2025-04-18]
 
 - `ryo3-jiter`
--- tests/fnv/test_fnv.py
+++ tests/fnv/test_fnv.py
@@ -16,13 +16,13 @@ def test_fnv_hasher_name() -> None:
 
 
 def test_fnv1a_empty() -> None:
-    assert ry.fnv1a(b"").digest() == 0xCBF29CE484222325
+    assert ry.fnv1a(b"").intdigest() == 0xCBF29CE484222325
 
 
 @pytest.mark.parametrize("input,expected", FNV_TEST_DATA)
 def test_fnv1a(input: bytes, expected: int) -> None:
     fnvhash = ry.fnv1a(input)
-    int_digest = fnvhash.digest()
+    int_digest = fnvhash.intdigest()
     assert int_digest == expected
     hex_str_expected = hex(expected)[2:]
 
@@ -38,9 +38,9 @@ def test_fnv1a(input: bytes, expected: int) -> None:
 def test_fnv1a_hasher(input: bytes, expected: int) -> None:
     thingy = ry.FnvHasher()
     thingy.update(input)
-    assert thingy.digest() == expected
+    assert thingy.intdigest() == expected
     thingy_with_init = ry.FnvHasher(input)
-    assert thingy_with_init.digest() == expected
+    assert thingy_with_init.intdigest() == expected
 
 
 def test_copy_hasher() -> None:
@@ -48,9 +48,9 @@ def test_copy_hasher() -> None:
     thingy.update(b"abc")
     thingy_copy = thingy.copy()
     thingy_copy.update(b"def")
-    assert thingy.digest() != thingy_copy.digest()
-    assert thingy_copy.digest() == ry.fnv1a(b"abcdef").digest()
-    r = thingy_copy.digest()
+    assert thingy.intdigest() != thingy_copy.intdigest()
+    assert thingy_copy.intdigest() == ry.fnv1a(b"abcdef").intdigest()
+    r = thingy_copy.intdigest()
     assert r is not None
     fnhashing = ry.fnv1a(b"abc")
     fnhashing.update(b"def")
