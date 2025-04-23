87e23950ff81a6eb0373e0cfde5bbaca09e5482d
jessekrubin <jessekrubin@gmail.com>
2025-04-23 10:03:11 -0700

    uuid setup and super super basic impl

---
+++ crates/ryo3-uuid/README.md
@@ -0,0 +1,8 @@
+# `ryo3-uuid`
+
+Wrapper around the `uuid` crate for Python.
+
+REF:
+
+- [crates.io](https://crates.io/crates/uuid)
+- [docs.rs](https://docs.rs/uuid)
---
+++ crates/ryo3-uuid/src/lib.rs
@@ -0,0 +1,11 @@
+#![doc = include_str!("../README.md")]
+mod py_uuid;
+pub use py_uuid::{PyUuid, uuid4};
+use pyo3::prelude::PyModule;
+use pyo3::prelude::*;
+
+pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<PyUuid>()?;
+    m.add_function(wrap_pyfunction!(uuid4, m)?)?;
+    Ok(())
+}
