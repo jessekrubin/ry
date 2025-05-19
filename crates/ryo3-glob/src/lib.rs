03895a10d61276e3782575541ff5e26e7a8e93a3
jessekrubin <jessekrubin@gmail.com>
2025-05-19 15:13:42 -0700

    glob dtype experiment

--- crates/ryo3-glob/src/lib.rs
+++ crates/ryo3-glob/src/lib.rs
@@ -3,15 +3,49 @@ mod pattern;
 
 use crate::pattern::PyPattern;
 use parking_lot::Mutex;
-use pyo3::prelude::*;
-use pyo3::types::PyModule;
+use pyo3::types::{PyModule, PyString, PyType, PyTypeMethods};
 use pyo3::IntoPyObjectExt;
+use pyo3::{prelude::*, PyTypeInfo};
+use std::path::PathBuf;
 use std::sync::Arc;
+
+enum GlobDType {
+    FsPath,
+    PathBuf,
+    OsString,
+}
+
+impl GlobDType {
+    fn into_bound_py_any<'py>(
+        &self,
+        py: Python<'py>,
+        path: PathBuf,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        match self {
+            GlobDType::FsPath => {
+                let fspath = ryo3_fspath::PyFsPath::from(path);
+                let any = fspath.into_bound_py_any(py)?;
+                Ok(any)
+            }
+            GlobDType::PathBuf => {
+                let any = path.into_bound_py_any(py)?;
+                Ok(any)
+            }
+            GlobDType::OsString => {
+                let os_string = path.into_os_string();
+                // let py_str = PyString::new(py, os_string.to_str().unwrap_or_default());
+                let any = os_string.into_bound_py_any(py)?;
+                Ok(any)
+            }
+        }
+    }
+}
 
 #[pyclass(name = "GlobPaths", module = "ry.ryo3", frozen)]
 pub struct PyGlobPaths {
     inner: Arc<Mutex<::glob::Paths>>,
     strict: bool,
+    dtype: GlobDType,
 }
 
 #[pymethods]
@@ -25,8 +59,11 @@ impl PyGlobPaths {
         while let Some(path) = self.inner.lock().next() {
             match path {
                 Ok(path) => {
-                    let pyany = path.into_bound_py_any(py)?;
+                    let pyany = self.dtype.into_bound_py_any(py, path)?;
                     return Ok(Some(pyany));
+
+                    //  path.into_bound_py_any(py)?;
+                    // return Ok(Some(pyany));
                 }
                 Err(e) => {
                     if self.strict {
@@ -46,8 +83,8 @@ impl PyGlobPaths {
             for path in self.inner.lock().by_ref() {
                 match path {
                     Ok(path) => {
-                        let py_any = path.into_bound_py_any(py)?;
-                        results.push(py_any);
+                        let any = self.dtype.into_bound_py_any(py, path)?;
+                        results.push(any);
                     }
                     Err(e) => {
                         return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(format!(
@@ -63,8 +100,8 @@ impl PyGlobPaths {
                 .by_ref()
                 .flatten()
                 .map(|path| {
-                    let el = path.into_bound_py_any(py)?;
-                    Ok(el)
+                    let py_any = self.dtype.into_bound_py_any(py, path)?;
+                    Ok(py_any)
                 })
                 .collect::<PyResult<Vec<_>>>()
         }
@@ -77,7 +114,7 @@ impl PyGlobPaths {
             for path_result in self.inner.lock().by_ref().take(n) {
                 let path = path_result
                     .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("{e}")))?;
-                let el = path.into_bound_py_any(py)?;
+                let el = self.dtype.into_bound_py_any(py, path)?;
                 results.push(el);
             }
 
@@ -89,8 +126,8 @@ impl PyGlobPaths {
                 .take(n)
                 .flatten()
                 .map(|path| {
-                    let el = path.into_bound_py_any(py)?;
-                    Ok(el)
+                    let py_any = self.dtype.into_bound_py_any(py, path)?;
+                    Ok(py_any)
                 })
                 .collect::<PyResult<Vec<_>>>()
         }
@@ -140,16 +177,20 @@ impl PyGlobPaths {
         case_sensitive=true,
         require_literal_separator=false,
         require_literal_leading_dot=false,
+        dtype=None,
         strict=true
     )
 )]
-pub fn py_glob(
+pub fn py_glob<'py>(
+    py: Python<'py>,
     pattern: &str,
     case_sensitive: bool,
     require_literal_separator: bool,
     require_literal_leading_dot: bool,
+    dtype: Option<Bound<'_, PyType>>,
     strict: bool,
 ) -> PyResult<PyGlobPaths> {
+    let dtype = extract_dtype(dtype)?;
     ::glob::glob_with(
         pattern,
         ::glob::MatchOptions {
@@ -161,8 +202,30 @@ pub fn py_glob(
     .map(|paths| PyGlobPaths {
         inner: Arc::new(Mutex::new(paths)),
         strict,
+        dtype,
     })
     .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(format!("{e}")))
+}
+
+fn extract_dtype<'py>(dtype: Option<Bound<'py, PyType>>) -> PyResult<GlobDType> {
+    match dtype {
+        Some(dtype) => {
+            let fully_qualified_name_pystr = dtype.fully_qualified_name()?;
+            let fully_qualified_name = fully_qualified_name_pystr.to_string();
+            if fully_qualified_name == "ry.ryo3.FsPath" {
+                return Ok(GlobDType::FsPath);
+            } else if fully_qualified_name == "pathlib.Path" {
+                return Ok(GlobDType::PathBuf);
+            } else if fully_qualified_name == "str" {
+                return Ok(GlobDType::OsString);
+            } else {
+                return Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(format!(
+                    "Invalid dtype: {fully_qualified_name} not supported"
+                )));
+            }
+        }
+        None => Ok(GlobDType::OsString),
+    }
 }
 
 pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
