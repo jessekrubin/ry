239554b40bb17717225b6a0f1d2e5f990ba96794
jessekrubin <jessekrubin@gmail.com>
2025-06-04 13:37:10 -0700

    fix clippy

--- crates/ryo3-json/src/lib.rs
+++ crates/ryo3-json/src/lib.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("../README.md")]
 use pyo3::prelude::*;
-use pyo3::types::{PyBytes, PyModule};
+use pyo3::types::PyModule;
 
 pub fn pymod_add(_m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
