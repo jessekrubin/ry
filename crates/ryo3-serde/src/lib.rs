d7d4a506963bc9f22138afa15333e83bd913bd8f
jessekrubin <jessekrubin@gmail.com>
2025-06-10 19:27:01 -0700

    fmt

--- crates/ryo3-serde/src/lib.rs
+++ crates/ryo3-serde/src/lib.rs
@@ -1,7 +1,6 @@
 #![doc = include_str!("../README.md")]
 use pyo3::prelude::*;
-use pyo3::types::{PyModule};
-
+use pyo3::types::PyModule;
 
 pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
