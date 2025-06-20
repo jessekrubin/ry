9811775062a4e932a6a5dfe461a67022aa91ef3f
jessekrubin <jessekrubin@gmail.com>
2025-06-20 12:36:04 -0700

    clippy fixing

--- crates/ryo3-jiff/src/serde.rs
+++ crates/ryo3-jiff/src/serde.rs
@@ -1,12 +1,11 @@
 use crate::RyTimeZone;
 use serde::Serialize;
-use std::ops::Deref;
 
 impl Serialize for RyTimeZone {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
     {
-        jiff::fmt::serde::tz::required::serialize(self.0.deref(), serializer)
+        jiff::fmt::serde::tz::required::serialize(&self.0, serializer)
     }
 }
