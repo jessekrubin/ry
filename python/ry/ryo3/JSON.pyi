edd1ab1955ba2cd0dbb7625657a3ae8c838ae86f
jessekrubin <jessekrubin@gmail.com>
2025-06-19 17:53:42 -0700

    json module!

--- python/ry/ryo3/JSON.pyi
+++ python/ry/ryo3/JSON.pyi
@@ -3,8 +3,11 @@
 from typing import Any, Callable, Literal, TypeVar, overload
 
 import typing_extensions
+import typing_extensions as te
 
+from ry._types import Buffer
 from ry.ryo3._bytes import Bytes
+from ry.ryo3._jiter import JsonParseKwargs
 
 JsonPrimitive: typing_extensions.TypeAlias = None | bool | int | float | str
 JsonValue: typing_extensions.TypeAlias = (
@@ -32,24 +35,14 @@ def stringify(
     pybytes: bool = False,
 ) -> Bytes: ...
 def parse_json(
-    data: bytes | str,
+    data: Buffer | bytes | str,
     /,
-    *,
-    allow_inf_nan: bool = True,
-    cache_mode: Literal[True, False, "all", "keys", "none"] = "all",
-    partial_mode: Literal[True, False, "off", "on", "trailing-strings"] = False,
-    catch_duplicate_keys: bool = False,
-    float_mode: Literal["float", "decimal", "lossless-float"] | bool = False,
+    **kwargs: te.Unpack[JsonParseKwargs],
 ) -> JsonValue: ...
 def parse_json_bytes(
-    data: bytes,
+    data: Buffer | bytes,
     /,
-    *,
-    allow_inf_nan: bool = True,
-    cache_mode: Literal[True, False, "all", "keys", "none"] = "all",
-    partial_mode: Literal[True, False, "off", "on", "trailing-strings"] = False,
-    catch_duplicate_keys: bool = False,
-    float_mode: Literal["float", "decimal", "lossless-float"] | bool = False,
+    **kwargs: te.Unpack[JsonParseKwargs],
 ) -> JsonValue: ...
-def json_cache_clear() -> None: ...
-def json_cache_usage() -> int: ...
+def cache_clear() -> None: ...
+def cache_usage() -> int: ...
