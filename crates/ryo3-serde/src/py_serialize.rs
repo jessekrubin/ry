b4f9f0cfb3d4e8ac1da65e20ca333529ca17d53d
jesse <jessekrubin@gmail.com>
2025-06-27 16:06:33 -0700

    dataclass support for serde-ing (#315)
    * dataclass support for serde-ing
    
    * string subclass

--- crates/ryo3-serde/src/py_serialize.rs
+++ crates/ryo3-serde/src/py_serialize.rs
@@ -26,8 +26,9 @@ use crate::rytypes;
 // #[cfg(feature = "ryo3-http")]
 // use crate::rytypes::{ry_headers, ry_http_status};
 use crate::type_cache::{PyObType, PyTypeCache};
-use pyo3::types::{PyAnyMethods, PyMapping, PySequence};
-use pyo3::Bound;
+use pyo3::sync::GILOnceCell;
+use pyo3::types::{PyAnyMethods, PyDict, PyMapping, PySequence, PyString};
+use pyo3::{intern, Bound};
 use serde::ser::SerializeSeq;
 
 type Depth = u8;
@@ -80,6 +81,15 @@ impl<'py> SerializePyAny<'py> {
             depth: self.depth + 1,
         }
     }
+}
+fn dataclass_fields<'a, 'py>(obj: &'a Bound<'py, PyAny>) -> Option<Bound<'py, PyDict>>
+where
+    'py: 'a, // keep lifetimes compatible
+{
+    obj.getattr("__dataclass_fields__") // PyResult<Bound<PyAny>>
+        .ok()? // Option<Bound<PyAny>>
+        .downcast_into::<PyDict>() // PyResult<Bound<PyDict>>
+        .ok() // Option<Bound<PyDict>>
 }
 
 impl Serialize for SerializePyAny<'_> {
@@ -147,6 +157,10 @@ impl Serialize for SerializePyAny<'_> {
                 #[cfg(feature = "ryo3-jiff")]
                 PyObType::RyZoned => rytypes::ry_zoned(self, serializer),
             }
+        } else if let Some(fields) = dataclass_fields(self.obj) {
+            let dc_serializer =
+                DataclassSerializer::new(self.obj, self.default, self.depth + 1, fields);
+            dc_serializer.serialize(serializer)
         } else if let Ok(py_map) = self.obj.downcast::<PyMapping>() {
             SerializePyMapping::new_with_depth(py_map, self.default, self.depth + 1)
                 .serialize(serializer)
@@ -245,6 +259,87 @@ impl Serialize for SerializePyMapping<'_, '_> {
             Err(S::Error::custom(
                 "SerializePyMapping: Length of mapping is not known.",
             ))
+        }
+    }
+}
+
+// ===========================================================================
+// DATACLASSES
+// ===========================================================================
+
+struct DataclassSerializer<'a, 'py> {
+    obj: &'a Bound<'py, PyAny>,
+    default: Option<&'py Bound<'py, PyAny>>,
+    fields: Bound<'py, PyDict>,
+    depth: Depth,
+}
+
+impl<'a, 'py> DataclassSerializer<'a, 'py> {
+    fn new(
+        obj: &'a Bound<'py, PyAny>,
+        default: Option<&'py Bound<'py, PyAny>>,
+        depth: Depth,
+        fields: Bound<'py, PyDict>,
+    ) -> Self {
+        Self {
+            obj,
+            default,
+            fields,
+            depth,
+        }
+    }
+}
+// as done in pydantic-core: https://github.com/pydantic/pydantic-core/blob/5f0b5a8b26691b7a1e3de07cb409b21bb174929c/src/serializers/shared.rs#L591
+static DC_FIELD_MARKER: GILOnceCell<PyObject> = GILOnceCell::new();
+/// needed to match the logic from dataclasses.fields `tuple(f for f in fields.values() if f._field_type is _FIELD)`
+fn get_field_marker(py: Python<'_>) -> PyResult<&Bound<'_, PyAny>> {
+    DC_FIELD_MARKER.import(py, "dataclasses", "_FIELD")
+}
+
+impl Serialize for DataclassSerializer<'_, '_> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if self.depth == MAX_DEPTH {
+            return Err(SerError::custom("recursion"));
+        }
+
+        // check for __dict__
+        if let Ok(dunder_dict) = self.obj.getattr("__dict__") {
+            if let Ok(dict) = dunder_dict.downcast_into::<PyDict>() {
+                // serialize the __dict__ as a dict
+                SerializePyAny::new_with_depth(&dict, self.default, self.depth + 1)
+                    .serialize(serializer)
+            } else {
+                serde_err!("__dict__ is not a dict")
+            }
+        } else {
+            let py = self.obj.py();
+            let field_marker = get_field_marker(py).map_err(pyerr2sererr)?;
+            let mut map = serializer.serialize_map(None)?;
+            for (field_name, field) in self.fields.iter() {
+                // check if the field is a dataclass field
+                let field_type = field
+                    .getattr(intern!(py, "_field_type"))
+                    .map_err(pyerr2sererr)?;
+                if field_type.is(field_marker) {
+                    // this is a dataclass field
+                    let field_name_py_str = field_name
+                        .downcast_into::<PyString>()
+                        .map_err(pyerr2sererr)?;
+                    let value = self.obj.getattr(&field_name_py_str).map_err(pyerr2sererr)?;
+                    let field_ser =
+                        SerializePyAny::new_with_depth(&value, self.default, self.depth + 1);
+
+                    // actual string
+                    let s = field_name_py_str
+                        .to_str()
+                        .map_err(|_| SerError::custom("field name is not a valid UTF-8 string"))?;
+                    map.serialize_entry(s, &field_ser)?;
+                }
+            }
+            map.end()
         }
     }
 }
