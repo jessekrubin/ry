b67f8677c8d3bfe5c55d556f65c7674e2f962128
jessekrubin <jessekrubin@gmail.com>
2025-07-03 14:26:26 -0700

    type layout

--- python/ry/ryo3/__init__.pyi
+++ python/ry/ryo3/__init__.pyi
@@ -1,9 +1,7 @@
 """ry api ~ type annotations"""
 
-from ry import dirs as dirs  # noqa: RUF100
 from ry import ulid as ulid  # noqa: RUF100
 from ry import uuid as uuid  # noqa: RUF100
-from ry import zstd as zstd  # noqa: RUF100
 from ry.ryo3._brotli import brotli as brotli
 from ry.ryo3._brotli import brotli_decode as brotli_decode
 from ry.ryo3._brotli import brotli_encode as brotli_encode
