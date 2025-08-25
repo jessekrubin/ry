0f0d0d76b599fd8194d80e6109089b7721ce929c
jesse <jessekrubin@gmail.com>
2025-08-25 13:41:04 -0700

    turn on hickory dns for request client (#366)

--- crates/ryo3-reqwest/src/user_agent.rs
+++ crates/ryo3-reqwest/src/user_agent.rs
@@ -2,13 +2,14 @@ use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use reqwest::header::HeaderValue;
 
+const DEFAULT_USER_AGENT: &str = concat!(
+    "ry/",
+    env!("CARGO_PKG_VERSION"),
+    " - OSS (github.com/jessekrubin/ry)"
+);
+
 pub(crate) fn parse_user_agent(user_agent: Option<String>) -> PyResult<HeaderValue> {
-    let ua_str = user_agent.unwrap_or_else(|| {
-        format!(
-            "ry/{} - OSS (github.com/jessekrubin/ry)",
-            env!("CARGO_PKG_VERSION")
-        )
-    });
+    let ua_str = user_agent.unwrap_or_else(|| DEFAULT_USER_AGENT.into());
     ua_str
         .parse()
         .map_err(|e| PyValueError::new_err(format!("{e}")))
