4d78572b64c818d3d1db71a84879cc90e9163f62
jessekrubin <jessekrubin@gmail.com>
2025-08-25 23:03:29 -0700

    fix: gz simplify

--- crates/ryo3-flate2/src/gz.rs
+++ crates/ryo3-flate2/src/gz.rs
@@ -1,5 +1,5 @@
 use flate2::Compression;
-use flate2::read::GzDecoder;
+use flate2::bufread::GzDecoder;
 use flate2::write::GzEncoder;
 use pyo3::prelude::*;
 use pyo3::types::PyBytes;
@@ -18,20 +18,14 @@ pub fn rs_gzip_encode(py: Python<'_>, data: &[u8], quality: Option<u32>) -> PyRe
         Compression::default()
     };
     let mut gzip_encoder = GzEncoder::new(Vec::new(), quality);
-    gzip_encoder
-        .write_all(data)
-        .map_err(|e| py_value_error!("gzip-decode-error: {e:?}"))?;
-    let encoded = gzip_encoder
-        .finish()
-        .map_err(|e| py_value_error!("gzip-decode-error: {e:?}"))?;
+    gzip_encoder.write_all(data)?;
+    let encoded = gzip_encoder.finish()?;
     Ok(PyBytes::new(py, &encoded).into())
 }
 
 pub fn rs_gzip_decode(data: &[u8]) -> PyResult<ryo3_bytes::PyBytes> {
     let mut decompressed = Vec::new();
-    GzDecoder::new(data)
-        .read_to_end(&mut decompressed)
-        .map_err(|e| py_value_error!("gzip-decode-error: {e:?}"))?;
+    GzDecoder::new(data).read_to_end(&mut decompressed)?;
     Ok(ryo3_bytes::PyBytes::from(decompressed))
 }
 
@@ -73,5 +67,5 @@ pub fn gunzip(data: ryo3_bytes::PyBytes) -> PyResult<ryo3_bytes::PyBytes> {
 #[expect(clippy::needless_pass_by_value)]
 pub fn is_gzipped(data: ryo3_bytes::PyBytes) -> bool {
     let bin: &[u8] = data.as_ref();
-    bin.starts_with(&[0x1F, 0x8B])
+    bin.starts_with(b"\x1F\x8B")
 }
