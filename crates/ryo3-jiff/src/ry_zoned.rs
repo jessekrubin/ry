ee27a3381ef4b3d0844a2ebd988b26118c2bba10
jessekrubin <jessekrubin@gmail.com>
2025-09-09 13:46:27 -0700

    rydantic checkpoint

--- crates/ryo3-jiff/src/ry_zoned.rs
+++ crates/ryo3-jiff/src/ry_zoned.rs
@@ -17,11 +17,11 @@ use crate::{
 use jiff::civil::{Date, Time, Weekday};
 use jiff::tz::{Offset, TimeZone};
 use jiff::{Zoned, ZonedDifference, ZonedRound};
+use pyo3::IntoPyObjectExt;
 use pyo3::exceptions::PyTypeError;
 use pyo3::prelude::*;
 use pyo3::pyclass::CompareOp;
 use pyo3::types::{PyTuple, PyType};
-use pyo3::IntoPyObjectExt;
 use ryo3_macro_rules::any_repr;
 use std::fmt::Display;
 use std::hash::{DefaultHasher, Hash, Hasher};
