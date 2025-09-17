f541d2e3e88a7af957f0ab44961d89f14636bf6c
jessekrubin <jessekrubin@gmail.com>
2025-09-17 11:19:05 -0700

    remove cls type param from classmethods

---
+++ test-cov.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# INSTALL STUFF WITH:
+# ```
+# rustup component add llvm-tools-preview
+# cargo install cargo-llvm-cov
+# ```
+
+source <(cargo llvm-cov show-env --export-prefix)
+
+export CARGO_TARGET_DIR=$CARGO_LLVM_COV_TARGET_DIR
+export CARGO_INCREMENTAL=1
+
+rm -rfv ./target/llvm-cov/html
+
+cargo llvm-cov clean --workspace
+# cargo test
+uv run -- maturin develop --uv
+uv run -- pytest tests/std --cov=ry --cov-report xml
+cargo llvm-cov report --lcov --output-path coverage.lcov
+cargo llvm-cov report --html
+serve ./target/llvm-cov/html
