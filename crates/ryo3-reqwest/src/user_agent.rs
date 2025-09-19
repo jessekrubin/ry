fd4013e72a95320de06040d15b4812d8fee05dd6
jessekrubin <jessekrubin@gmail.com>
2025-09-18 20:59:58 -0700

    bump version and remove url from default user-agent

--- crates/ryo3-reqwest/src/user_agent.rs
+++ crates/ryo3-reqwest/src/user_agent.rs
@@ -2,11 +2,7 @@ use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use reqwest::header::HeaderValue;
 
-const DEFAULT_USER_AGENT: &str = concat!(
-    "ry/",
-    env!("CARGO_PKG_VERSION"),
-    " - OSS (github.com/jessekrubin/ry)"
-);
+const DEFAULT_USER_AGENT: &str = concat!("ry/", env!("CARGO_PKG_VERSION"),);
 
 pub(crate) fn parse_user_agent(user_agent: Option<String>) -> PyResult<HeaderValue> {
     let ua_str = user_agent.unwrap_or_else(|| DEFAULT_USER_AGENT.into());
