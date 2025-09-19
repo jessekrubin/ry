1611efdffc15f5c0ee95cc2d1352a74803ecf819
jessekrubin <jessekrubin@gmail.com>
2025-09-18 18:06:11 -0700

    fix typo

--- tests/test_build_profile.py
+++ tests/test_build_profile.py
@@ -6,5 +6,5 @@ import ry
 def test_check_build_profile() -> None:
     assert ry.__build_profile__ is not None
     assert ry.__build_profile__ in {"debug", "release"}, (
-        f"utiles.__build_profile__ is not 'debug'/'release': {ry.__build_profile__}"
+        f"ry.__build_profile__ is not 'debug'/'release': {ry.__build_profile__}"
     )
