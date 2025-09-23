a64a65e28cd4d7c401d719e3b8f8ecd3f6154f2d
jesse <jessekrubin@gmail.com>
2025-09-22 21:48:47 -0700

    test: cov test (#425)
    * tiny PR to see if the codecov action is dope/dumb
    
    * ok remove redundant build from cov wf

--- tests/jiff/test_date.py
+++ tests/jiff/test_date.py
@@ -27,3 +27,13 @@ class TestDateUntil:
         earlier = ry.date(2006, 8, 24)
         later = ry.date(2019, 1, 31)
         assert later.until(earlier) == ry.timespan(days=-4543)
+
+
+class TestDateTomorrowYesterday:
+    def test_date_tomorrow(self) -> None:
+        d = ry.date(2023, 3, 14)
+        assert d.tomorrow() == ry.date(2023, 3, 15)
+
+    def test_date_yesterday(self) -> None:
+        d = ry.date(2023, 3, 14)
+        assert d.yesterday() == ry.date(2023, 3, 13)
