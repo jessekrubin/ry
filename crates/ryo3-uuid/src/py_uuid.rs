c3606bd6e071f190a75bd9ea4e9b426c60fff0e1
jessekrubin <jessekrubin@gmail.com>
2025-10-13 11:16:35 -0700

    fix time in uuid

--- crates/ryo3-uuid/src/py_uuid.rs
+++ crates/ryo3-uuid/src/py_uuid.rs
@@ -340,10 +340,31 @@ impl PyUuid {
     }
 
     #[getter]
+    /// The 60-bit timestamp as a count of 100-nanosecond intervals since
+    /// Gregorian epoch (1582-10-15 00:00:00) for versions 1 and 6, or the
+    /// 48-bit timestamp in milliseconds since Unix epoch (1970-01-01 00:00:00)
+    /// for version 7.
     fn time(&self) -> u64 {
-        let high = u64::from(self.time_hi_version()) & 0x0fff;
-        let mid = u64::from(self.time_mid());
-        high.wrapping_shl(48) | mid.wrapping_shl(32) | u64::from(self.time_low())
+        let version_rfc = (
+            self.version(),
+            matches!(self.0.get_variant(), uuid::Variant::RFC4122),
+        );
+        match version_rfc {
+            (6, true) => {
+                let high32 = (u64::from(self.time_low())) << 28;
+                let mid16 = (u64::from(self.time_mid())) << 12;
+                let low12 = u64::from(self.time_hi_version()) & 0x0fff;
+                high32 | mid16 | low12
+            }
+            (7, true) => (self.0.as_u128() >> 80) as u64,
+            // should be 1 and/or any other versions but idk if it is actually
+            // implemented?
+            _ => {
+                let high = u64::from(self.time_hi_version()) & 0x0fff;
+                let mid = u64::from(self.time_mid());
+                high.wrapping_shl(48) | mid.wrapping_shl(32) | u64::from(self.time_low())
+            }
+        }
     }
 
     #[getter]
