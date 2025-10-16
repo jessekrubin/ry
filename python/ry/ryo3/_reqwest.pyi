357110aa4682a1a7f741da87b5a2324d22612833
jessekrubin <jessekrubin@gmail.com>
2025-10-15 17:07:30 -0700

    root certs

--- python/ry/ryo3/_reqwest.pyi
+++ python/ry/ryo3/_reqwest.pyi
@@ -57,6 +57,7 @@ class HttpClient:
         tcp_keepalive_interval: Duration | None = ...,  # 15 seconds
         tcp_keepalive_retries: int | None = 3,
         tcp_nodelay: bool = True,
+        root_certs: list[Certificate] | None = None,
         tls_min_version: t.Literal["1.0", "1.1", "1.2", "1.3"] | None = None,
         tls_max_version: t.Literal["1.0", "1.1", "1.2", "1.3"] | None = None,
         tls_info: bool = False,
