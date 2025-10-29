18bfbdacdbaf55dd9061d9f34097e7080ff0cb5a
jessekrubin <jessekrubin@gmail.com>
2025-10-29 07:20:13 -0700

    series rework

--- crates/ryo3-jiff/src/series.rs
+++ crates/ryo3-jiff/src/series.rs
@@ -2,36 +2,91 @@ use crate::{RyDate, RyDateTime, RyTime, RyTimestamp};
 use parking_lot::Mutex;
 use pyo3::prelude::*;
 
-#[pyclass(name = "DateSeries", frozen)]
-#[cfg_attr(feature = "ry", pyo3(module = "ry.ryo3"))]
-pub struct RyDateSeries {
-    pub(crate) series: Mutex<jiff::civil::DateSeries>,
-}
 
-impl From<jiff::civil::DateSeries> for RyDateSeries {
-    fn from(series: jiff::civil::DateSeries) -> Self {
-        Self {
-            series: Mutex::new(series),
+macro_rules! define_py_jiff_series {
+    (
+        py_name: $py_name:literal,
+        ry_name: $ry_name:ident,
+        series_type: $jiff_series_type:ty,
+        jiff_item_type: $jiff_item_type:ty,
+        ry_item_type: $ry_item_type:ty
+    ) => {
+        #[pyclass(name = $py_name, frozen)]
+        #[cfg_attr(feature = "ry", pyo3(module = "ry.ryo3"))]
+        pub struct $ry_name {
+            pub(crate) series: Mutex<$jiff_series_type>,
         }
-    }
+
+        impl From<$jiff_series_type> for $ry_name {
+            fn from(series: $jiff_series_type) -> Self {
+                Self {
+                    series: Mutex::new(series),
+                }
+            }
+        }
+
+        #[pymethods]
+        impl $ry_name {
+            fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+                slf
+            }
+
+            fn __next__(&self) -> Option<$ry_item_type> {
+                self.series.lock().next().map(Into::into)
+            }
+
+            #[pyo3(signature = (n = 1))]
+            fn take(&self, n: usize) -> Vec<$ry_item_type> {
+                let mut s = self.series.lock();
+                s.by_ref().take(n).map(Into::into).collect()
+            }
+        }
+
+    };
 }
 
-#[pymethods]
-impl RyDateSeries {
-    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
-        slf
-    }
+define_py_jiff_series!(
+    py_name: "DateSeries",
+    ry_name: RyDateSeries,
+    series_type: jiff::civil::DateSeries,
+    jiff_item_type: jiff::civil::Date,
+    ry_item_type: RyDate
+    // "DateSeries",
+    // RyDateSeries,
+    // jiff::civil::DateSeries,
+    // jiff::civil::Date,
+    // RyDate
+);
+// #[pyclass(name = "DateSeries", frozen)]
+// #[cfg_attr(feature = "ry", pyo3(module = "ry.ryo3"))]
+// pub struct RyDateSeries {
+//     pub(crate) series: Mutex<jiff::civil::DateSeries>,
+// }
 
-    fn __next__(&self) -> Option<RyDate> {
-        self.series.lock().next().map(RyDate::from)
-    }
+// impl From<jiff::civil::DateSeries> for RyDateSeries {
+//     fn from(series: jiff::civil::DateSeries) -> Self {
+//         Self {
+//             series: Mutex::new(series),
+//         }
+//     }
+// }
 
-    #[pyo3(signature = (n = 1))]
-    fn take(&self, n: usize) -> Vec<RyDate> {
-        let mut s = self.series.lock();
-        s.by_ref().take(n).map(RyDate::from).collect()
-    }
-}
+// #[pymethods]
+// impl RyDateSeries {
+//     fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
+//         slf
+//     }
+
+//     fn __next__(&self) -> Option<RyDate> {
+//         self.series.lock().next().map(RyDate::from)
+//     }
+
+//     #[pyo3(signature = (n = 1))]
+//     fn take(&self, n: usize) -> Vec<RyDate> {
+//         let mut s = self.series.lock();
+//         s.by_ref().take(n).map(RyDate::from).collect()
+//     }
+// }
 
 #[pyclass(name = "DateTimeSeries", frozen)]
 #[cfg_attr(feature = "ry", pyo3(module = "ry.ryo3"))]
