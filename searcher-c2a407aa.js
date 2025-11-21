16fdb13419495adbe5d9e09dd33291b38e44057d
jessekrubin <jessekrubin@users.noreply.github.com>
2025-11-21 20:24:28 +0000

    deploy: 6fce13652918340cd300ae55375fe4dc8a88298e

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-615d5237.js',
+                path_to_root + 'searchindex-372e2aad.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
