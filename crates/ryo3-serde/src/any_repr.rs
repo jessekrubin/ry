c9ef83991774e586d07cb80d99c03f8c3ce2f415
jessekrubin <jessekrubin@gmail.com>
2025-11-24 14:00:20 -0800

    aha keys for dicts

--- crates/ryo3-serde/src/any_repr.rs
+++ crates/ryo3-serde/src/any_repr.rs
@@ -1,13 +1,10 @@
 use pyo3::prelude::*;
 use pyo3::types::PyString;
 
+#[inline]
 pub(crate) fn any_repr(obj: &Bound<'_, PyAny>) -> String {
-    let typ = obj.get_type();
-    let name = typ
+    obj.get_type()
         .name()
-        .unwrap_or_else(|_| PyString::new(obj.py(), "unknown"));
-    match obj.repr() {
-        Ok(repr) => format!("{repr} ({name})"),
-        Err(_) => name.to_string(),
-    }
+        .unwrap_or_else(|_| PyString::new(obj.py(), "unknown"))
+        .to_string()
 }
