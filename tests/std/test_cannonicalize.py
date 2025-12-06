2727bc5d94514f3fe4e9b9c3b5b1c68471889d60
jessekrubin <jessekrubin@gmail.com>
2025-12-06 13:17:59 -0800

    cannonicalize fix

--- tests/std/test_cannonicalize.py
+++ tests/std/test_cannonicalize.py
@@ -4,12 +4,14 @@ import ry
 
 _UNCANNONICAL_PATH = "tests/std/../../tests/std/test_cannonicalize.py"
 
+_EXPECTED_PATH_TRAILER = pathlib.Path("tests/std/test_cannonicalize.py")
+
 
 def test_canonicalize_str() -> None:
     cannon = ry.canonicalize(_UNCANNONICAL_PATH)
-    assert cannon.endswith("tests/std/test_cannonicalize.py")
+    assert cannon.endswith(str(_EXPECTED_PATH_TRAILER))
 
 
 def test_canonicalize_path() -> None:
     cannon = ry.canonicalize(pathlib.Path(_UNCANNONICAL_PATH))
-    assert str(cannon).endswith("tests/std/test_cannonicalize.py")
+    assert str(cannon).endswith(str(_EXPECTED_PATH_TRAILER))
