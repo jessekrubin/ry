50862daf3c5af7a774778c5a1795da1165ea19fa
jessekrubin <jessekrubin@gmail.com>
2026-01-31 15:25:38 -0800

    update types

--- python/ry/ryo3/_jiff.pyi
+++ python/ry/ryo3/_jiff.pyi
@@ -228,14 +228,45 @@ class Date(
     # =========================================================================
     # ARITHMETIC METHODS
     # =========================================================================
+    @t.overload
     def add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> Date: ...
+    @t.overload
+    def add(
+        self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> Date: ...
     @t.overload
     def sub(self, other: Date) -> TimeSpan: ...
     @t.overload
     def sub(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> Date: ...
+    @t.overload
+    def sub(
+        self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> Date: ...
     def saturating_add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
@@ -408,10 +439,6 @@ class Time(
     def add(
         self,
         *,
-        years: int = 0,
-        months: int = 0,
-        weeks: int = 0,
-        days: int = 0,
         hours: int = 0,
         minutes: int = 0,
         seconds: int = 0,
@@ -424,6 +451,17 @@ class Time(
     @t.overload
     def sub(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> t.Self: ...
+    @t.overload
+    def sub(
+        self,
+        *,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> t.Self: ...
     def saturating_add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
@@ -578,8 +616,24 @@ class DateTime(
     # =========================================================================
     # ARITHMETIC METHODS
     # =========================================================================
+    @t.overload
     def add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> t.Self: ...
+    @t.overload
+    def add(
+        self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> t.Self: ...
     @t.overload
     def sub(self, other: t.Self) -> TimeSpan: ...
@@ -588,8 +642,20 @@ class DateTime(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
     ) -> t.Self: ...
     @t.overload
-    def saturating_sub(self, other: t.Self) -> TimeSpan: ...
-    @t.overload
+    def sub(
+        self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
+    ) -> t.Self: ...
     def saturating_sub(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
     ) -> t.Self: ...
@@ -1221,14 +1287,37 @@ class Timestamp(
     # =========================================================================
     # ARITHMETIC METHODS
     # =========================================================================
+    @t.overload
     def add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> t.Self: ...
+    @t.overload
+    def add(
+        self,
+        *,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> t.Self: ...
     @t.overload
     def sub(self, other: t.Self) -> TimeSpan: ...
     @t.overload
     def sub(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> t.Self: ...
+    @t.overload
+    def sub(
+        self,
+        *,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> t.Self: ...
     def saturating_add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
@@ -1555,8 +1644,24 @@ class ZonedDateTime(
     # =========================================================================
     # ARITHMETIC METHODS
     # =========================================================================
+    @t.overload
     def add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
+    ) -> t.Self: ...
+    @t.overload
+    def add(
+        self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
     ) -> t.Self: ...
     @t.overload
     def sub(self, other: t.Self) -> TimeSpan: ...
@@ -1564,12 +1669,24 @@ class ZonedDateTime(
     def sub(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
     ) -> t.Self: ...
+    @t.overload
+    def sub(
+        self,
+        *,
+        years: int = 0,
+        months: int = 0,
+        weeks: int = 0,
+        days: int = 0,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+        milliseconds: int = 0,
+        microseconds: int = 0,
+        nanoseconds: int = 0,
+    ) -> t.Self: ...
     def saturating_add(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
     ) -> t.Self: ...
-    @t.overload
-    def saturating_sub(self, other: t.Self) -> TimeSpan: ...
-    @t.overload
     def saturating_sub(
         self, other: TimeSpan | SignedDuration | Duration | pydt.timedelta
     ) -> t.Self: ...
@@ -1815,11 +1932,29 @@ class Offset(
     # =========================================================================
     # ARITHMETIC METHODS
     # =========================================================================
+    @t.overload
     def add(
         self, other: Duration | SignedDuration | TimeSpan | pydt.timedelta
     ) -> t.Self: ...
+    @t.overload
+    def add(
+        self,
+        *,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
+    ) -> t.Self: ...
+    @t.overload
     def sub(
         self, other: Duration | SignedDuration | TimeSpan | pydt.timedelta
+    ) -> t.Self: ...
+    @t.overload
+    def sub(
+        self,
+        *,
+        hours: int = 0,
+        minutes: int = 0,
+        seconds: int = 0,
     ) -> t.Self: ...
     def saturating_add(
         self, other: Duration | SignedDuration | TimeSpan | pydt.timedelta
---
+++ tests/jiff/test_add_sub.py
@@ -0,0 +1,55 @@
+from __future__ import annotations
+
+import pytest
+
+import ry
+
+
+@pytest.mark.parametrize(
+    "value",
+    [
+        # time
+        ry.time(14, 30, 0, 0),
+        # date
+        ry.date(2020, 8, 26),
+        # time
+        ry.time(6, 27, 0, 0),
+        # datetime
+        ry.datetime(2020, 8, 26, 6, 27, 0, 0),
+        # timestamp
+        ry.Timestamp.from_millisecond(1598438400000),
+        # Zoned
+        ry.datetime(2020, 8, 26, 6, 27, 0, 0).in_tz("America/New_York"),
+        # signed-duration
+        ry.Offset(seconds=90),
+    ],
+)
+@pytest.mark.parametrize(
+    "kw",
+    [
+        {"years": 1},
+        {"months": 1},
+        {"weeks": 1},
+        {"days": 1},
+        {"hours": 1},
+        {"minutes": 1},
+        {"seconds": 1},
+        {"milliseconds": 1},
+        {"microseconds": 1},
+        {"nanoseconds": 1},
+    ],
+)
+def test_add_sub(
+    value: ry.Time | ry.DateTime | ry.Date | ry.Timestamp | ry.ZonedDateTime,
+    kw: dict[str, int],
+) -> None:
+    if isinstance(value, (ry.Time, ry.Timestamp, ry.Offset)) and any(
+        v in {"years", "months", "weeks", "days"} for v in kw
+    ):
+        with pytest.raises(TypeError, match="got an unexpected keyword argument"):
+            _ = value.add(**kw)
+        with pytest.raises(TypeError, match="got an unexpected keyword argument"):
+            _ = value.sub(**kw)
+        return
+    assert isinstance(value.add(**kw), type(value))
+    assert isinstance(value.sub(**kw), type(value))
