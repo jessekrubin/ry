102d60b58a78bbef6151e9902958b322962d95e6
jessekrubin <jessekrubin@gmail.com>
2026-05-06 14:24:55 -0700

    boom semaphore

--- crates/ryo3-tokio/src/lib.rs
+++ crates/ryo3-tokio/src/lib.rs
@@ -2,18 +2,26 @@
 
 #[cfg(feature = "fs")]
 pub mod fs;
+#[cfg(feature = "sync")]
+pub mod sync;
 #[cfg(feature = "time")]
 pub mod time;
 
 use pyo3::prelude::*;
 
-#[cfg_attr(not(any(feature = "fs", feature = "time")), expect(unused_variables))]
+#[cfg_attr(
+    not(any(feature = "fs", feature = "time", feature = "sync")),
+    expect(unused_variables)
+)]
 pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
     #[cfg(feature = "time")]
     time::pymod_add(m)?;
 
     #[cfg(feature = "fs")]
     fs::pymod_add(m)?;
+
+    #[cfg(feature = "sync")]
+    sync::pymod_add(m)?;
 
     Ok(())
 }
