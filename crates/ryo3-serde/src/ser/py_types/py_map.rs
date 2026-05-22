44358d4ba33daaf5efcfa75991b14087f14edc0f
jessekrubin <jessekrubin@gmail.com>
2026-05-22 16:05:02 -0700

    okee

--- crates/ryo3-serde/src/ser/py_types/py_map.rs
+++ crates/ryo3-serde/src/ser/py_types/py_map.rs
@@ -284,8 +284,8 @@ impl Serialize for PyDictSerializer<'_, '_> {
         let mut prev_ob_type_ptr = 0;
         let mut prev_ob_type = PyObType::Unknown;
 
+        #[cfg(not(Py_GIL_DISABLED))]
         for (map_key, map_val) in BorrowedDictIter::new(self.obj) {
-            let sk = PyMappingKeySerializer::new(self.ctx, map_key);
             let type_ptr = map_val.get_type_ptr() as usize;
             let ob_type = if type_ptr == prev_ob_type_ptr {
                 prev_ob_type
@@ -295,6 +295,27 @@ impl Serialize for PyDictSerializer<'_, '_> {
                 prev_ob_type = t;
                 t
             };
+            let sk = PyMappingKeySerializer::new(self.ctx, map_key);
+            m.serialize_key(&sk)?;
+            serialize_map_value!(ob_type, m, self, map_val);
+        }
+
+        // idk wtf would happen with freethreaded and the borrowed thing so
+        // les just use the old one for this... ?
+        #[cfg(Py_GIL_DISABLED)]
+        for (map_key, map_val) in self.obj.iter() {
+            let map_key = map_key.as_borrowed();
+            let map_val = map_val.as_borrowed();
+            let type_ptr = map_val.get_type_ptr() as usize;
+            let ob_type = if type_ptr == prev_ob_type_ptr {
+                prev_ob_type
+            } else {
+                let t = self.ctx.typeref.ptr2type(type_ptr);
+                prev_ob_type_ptr = type_ptr;
+                prev_ob_type = t;
+                t
+            };
+            let sk = PyMappingKeySerializer::new(self.ctx, map_key);
             m.serialize_key(&sk)?;
             serialize_map_value!(ob_type, m, self, map_val);
         }
