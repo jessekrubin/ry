4fa68b03a78070e30e4f75d7e1a6b28066e06b4f
jessekrubin <jessekrubin@gmail.com>
2026-06-02 21:56:52 -0700

    ahoy!

--- crates/ryo3-core/src/types/utf8_bytes.rs
+++ crates/ryo3-core/src/types/utf8_bytes.rs
@@ -1,6 +1,8 @@
-use pyo3::IntoPyObjectExt;
 use pyo3::exceptions::PyUnicodeDecodeError;
+#[cfg(feature = "experimental-inspect")]
+use pyo3::inspect::PyStaticExpr;
 use pyo3::prelude::*;
+use pyo3::types::PyString;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct PyUtf8Bytes(Vec<u8>);
@@ -27,14 +29,17 @@ impl From<Vec<u8>> for PyUtf8Bytes {
 }
 
 impl<'py> IntoPyObject<'py> for PyUtf8Bytes {
-    type Target = PyAny;
+    type Target = PyString;
     type Output = Bound<'py, Self::Target>;
     type Error = PyErr;
+
+    #[cfg(feature = "experimental-inspect")]
+    const OUTPUT_TYPE: PyStaticExpr = String::OUTPUT_TYPE;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> PyResult<Self::Output> {
         match std::str::from_utf8(&self.0) {
-            Ok(s) => s.into_bound_py_any(py),
+            Ok(s) => Ok(PyString::new(py, s)),
             Err(e) => Err(PyUnicodeDecodeError::new_utf8(py, &self.0, e)?.into()),
         }
     }
