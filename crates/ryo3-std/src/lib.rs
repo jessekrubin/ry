eb1ac418fb75c0731a80c9a7e4ea7e47584af773
jesse <jessekrubin@gmail.com>
2026-06-12 17:37:37 -0700

    pyo3 v0.29! (#684)
    * pyo3-0.29
    
    * update maturin
    
    * maturin!
    
    * use actual pyo3-async-runtimes
    
    * update and pin time crate
    
    * end of file
    
    * donezo

--- crates/ryo3-std/src/lib.rs
+++ crates/ryo3-std/src/lib.rs
@@ -1,14 +1,15 @@
 use pyo3::prelude::*;
-
 #[cfg(feature = "fs")]
 pub mod fs;
 #[cfg(feature = "net")]
 pub mod net;
+pub mod primitive;
 #[cfg(feature = "time")]
 pub mod time;
 
 #[allow(unused_variables)]
 pub fn pymod_add(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    primitive::pymod_add(m)?;
     #[cfg(feature = "fs")]
     fs::pymod_add(m)?;
     #[cfg(feature = "net")]
