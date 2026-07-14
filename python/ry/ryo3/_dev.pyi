7bc3fcc1d31bbf43cb2375fa3f6a65253bc17b44
jessekrubin <jessekrubin@gmail.com>
2026-07-14 09:15:53 -0700

    fix up the dev api

--- python/ry/ryo3/_dev.pyi
+++ python/ry/ryo3/_dev.pyi
@@ -2,11 +2,4 @@
 
 import typing as t
 
-from ry.ryo3._lz4rip import Lz4FrameInfo as Lz4FrameInfo
-from ry.ryo3._lz4rip import lz4_compress as lz4_compress
-from ry.ryo3._lz4rip import lz4_compress_block as lz4_compress_block
-from ry.ryo3._lz4rip import lz4_decompress as lz4_decompress
-from ry.ryo3._lz4rip import lz4_decompress_block as lz4_decompress_block
-from ry.ryo3._lz4rip import lz4_train_dict as lz4_train_dict
-
 def devfn() -> t.Literal["_ryo3-dev"]: ...
