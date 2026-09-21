ad7edb39e310a96149f14597aaa234fa8b69f827
jessekrubin <jessekrubin@gmail.com>
2026-09-21 10:21:32 -0700

    fix crates

--- crates/ryo3-reqwest/src/request.rs
+++ crates/ryo3-reqwest/src/request.rs
@@ -149,7 +149,9 @@ fn extract_kwargs<const BLOCKING: bool>(
         (Some(body), None, None, None) => {
             extract_body_from_py_body::<BLOCKING>(body.as_borrowed())?
         }
-        (None, Some(json), None, None) => PyReqwestBody::Json(ryo3_json::to_vec(&json)?),
+        (None, Some(json), None, None) => {
+            PyReqwestBody::Json(ryo3_json::to_vec(json.as_borrowed())?)
+        }
         (None, None, Some(form), None) => extract_form_body(form.as_borrowed())?,
         (None, None, None, Some(_multipart)) => pytodo!("multipart not implemented (yet)"),
         (None, None, None, None) => PyReqwestBody::None,
