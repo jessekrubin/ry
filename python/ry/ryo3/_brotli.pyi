103b26e6ae0bc6670c5ac4f0de55a38f7c550604
jessekrubin <jessekrubin@gmail.com>
2026-09-25 16:43:53 -0700

    rename quality

--- python/ry/ryo3/_brotli.pyi
+++ python/ry/ryo3/_brotli.pyi
@@ -5,13 +5,13 @@ import typing as t
 from ry._types import Buffer
 from ry.ryo3._bytes import Bytes
 
-_Quality: t.TypeAlias = t.Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+_BrotliQuality: t.TypeAlias = t.Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
 def brotli_encode(
-    data: Buffer, quality: _Quality = 11, *, magic_number: bool = False
+    data: Buffer, quality: _BrotliQuality = 11, *, magic_number: bool = False
 ) -> Bytes: ...
 def brotli_decode(data: Buffer) -> Bytes: ...
 def brotli(
-    data: Buffer, quality: _Quality = 11, *, magic_number: bool = False
+    data: Buffer, quality: _BrotliQuality = 11, *, magic_number: bool = False
 ) -> Bytes:
     """Alias for brotli_encode"""
--- python/ry/ryo3/_flate2.pyi
+++ python/ry/ryo3/_flate2.pyi
@@ -5,11 +5,11 @@ import typing as t
 from ry import Bytes
 from ry._types import Buffer
 
-_Quality: t.TypeAlias = t.Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "best", "fast"]
+_GzipQuality: t.TypeAlias = t.Literal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "best", "fast"]
 
-def gzip_encode(data: Buffer, quality: _Quality = 6) -> Bytes: ...
+def gzip_encode(data: Buffer, quality: _GzipQuality = 6) -> Bytes: ...
 def gzip_decode(data: Buffer) -> Bytes: ...
-def gzip(data: Buffer, quality: _Quality = 6) -> Bytes:
+def gzip(data: Buffer, quality: _GzipQuality = 6) -> Bytes:
     """Alias for gzip_encode"""
 
 def gunzip(data: Buffer) -> Bytes:
